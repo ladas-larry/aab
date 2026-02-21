d193465ad237b9fd427c46fd71ef70c2eab14c0d
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-02-21 15:44:37 +0100

    tests: Add test for abmeldung-email

---
+++ tests/tools/abmeldung_email/test_abmeldung_email.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture
+def abmeldung_email_generator(page):
+    page.goto("/tests/tools/abmeldung-email")
+    return page.get_by_role("group", name="Letter generator to deregister your address")
+
+
+def test_snapshot(page, abmeldung_email_generator, test_screenshot):
+    tool = abmeldung_email_generator
+    test_screenshot(page, tool)
+    tool.get_by_role("button", name="Customize").click()
+    tool.get_by_label("Your full name").fill("John Smith")
+    tool.get_by_label("Your new address").fill("123 Maple Street\nMontreal, Quebec\nCanada")
+    tool.get_by_label("Backup recipient").fill("Bella Johnson")
+    tool.get_by_label("Backup address").fill("Wohnungstr. 123\n13122 Berlin")
+    tool.get_by_role("button", name="Preview").click()
+    test_screenshot(page, tool)
